77054ff7301fa82e45f77383b84ae6bbc5c8550f
roian6 <roian6@naver.com>
2020-08-10 11:54:08 +0900

    login

---
+++ app/src/main/java/com/david0926/edcansummer/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.david0926.edcansummer;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.david0926.edcansummer.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        binding.setEmail("");
+        binding.setPw("");
+
+        binding.btnLoginSignin.setOnClickListener(view -> {
+            login(binding.getEmail(), binding.getPw());
+        });
+    }
+
+    private void login(String email, String pw){
+        Toast.makeText(this, email+" "+pw, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/david0926/edcansummer/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.david0926.edcansummer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
+        }, 1500);
+    }
+}
