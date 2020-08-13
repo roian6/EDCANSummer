c2f295dfaec397c53e5980ba3ba0bf9d61000432
roian6 <roian6@naver.com>
2020-08-13 10:41:17 +0900

    upload memo finish

--- app/src/main/java/com/david0926/edcansummer/MemoFragment.java
+++ app/src/main/java/com/david0926/edcansummer/MemoFragment.java
@@ -1,10 +1,12 @@
 package com.david0926.edcansummer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,13 +16,16 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.david0926.edcansummer.databinding.FragmentMemoBinding;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MemoFragment extends Fragment {
 
-    public static MemoFragment newInstance(){
+    public static MemoFragment newInstance() {
         return new MemoFragment();
     }
 
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     private ObservableArrayList<MemoModel> items = new ObservableArrayList<>();
 
     private Context mContext;
@@ -44,19 +49,27 @@ public class MemoFragment extends Fragment {
 
         binding.setItems(items);
 
-        MemoModel model1 = new MemoModel();
-        model1.setEmail("");
-        model1.setText("테스트 내용 1");
-        model1.setTime("대충 작성된 시간");
-
-        MemoModel model2 = new MemoModel();
-        model2.setEmail("");
-        model2.setText("테스트 내용 2");
-        model2.setTime("대충 작성된 시간");
-
-        items.add(model1);
-        items.add(model2);
+        binding.fabMemo.setOnClickListener(view -> startActivity(new Intent(mContext, NewMemoActivity.class)));
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getMemos();
+    }
+
+    private void getMemos() {
+        items.clear();
+        firebaseFirestore
+                .collection("memo")
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    for(DocumentSnapshot d : queryDocumentSnapshots){
+                        items.add(d.toObject(MemoModel.class));
+                    }
+                })
+                .addOnFailureListener(e -> Toast.makeText(mContext, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show());
     }
 }
--- app/src/main/java/com/david0926/edcansummer/NewMemoActivity.java
+++ app/src/main/java/com/david0926/edcansummer/NewMemoActivity.java
@@ -1,13 +1,21 @@
 package com.david0926.edcansummer;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.os.Bundle;
-
 import com.david0926.edcansummer.databinding.ActivityNewMemoBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class NewMemoActivity extends AppCompatActivity {
+
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
 
     private ActivityNewMemoBinding binding;
 
@@ -20,8 +28,30 @@ public class NewMemoActivity extends AppCompatActivity {
         binding.toolbarNewMemo.setNavigationOnClickListener(view -> finish());
 
         binding.btnNewMemoUpload.setOnClickListener(view -> {
-            //
+            if (binding.getMemo().isEmpty()) {
+                Toast.makeText(this, "빈칸을 입력해주세요", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            MemoModel model = new MemoModel();
+            model.setEmail(UserCache.getUser(this).getEmail());
+            model.setText(binding.getMemo());
+            model.setTime(getTime());
+
+            firebaseFirestore
+                    .collection("memo")
+                    .document()
+                    .set(model)
+                    .addOnSuccessListener(runnable -> {
+                        Toast.makeText(this, "정상적으로 등록되었습니다!", Toast.LENGTH_SHORT).show();
+                        finish();
+                    })
+                    .addOnFailureListener(e -> Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show());
         });
 
+    }
+
+    private String getTime() {
+        return new SimpleDateFormat("yyyy/MM/dd hh:mm aa", Locale.ENGLISH).format(new Date());
     }
 }
